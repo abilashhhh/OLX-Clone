ebcaec7fc2eddee443f91b5201ec92a3feee88db
Abilash N <103725378+abilashhhh@users.noreply.github.com>
2024-03-20 20:06:24 +0530

    Removed Congig's

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
  
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBUtzizY3AdlZpxOMmaDwByqZf9Ni9mLqw",
-    authDomain: "olx-clone-829cd.firebaseapp.com",
-    projectId: "olx-clone-829cd",
-    storageBucket: "olx-clone-829cd.appspot.com",
-    messagingSenderId: "520508071664",
-    appId: "1:520508071664:web:7e871606f238fd94af6c35",
-    measurementId: "G-6R8WVQY62T"
+    apiKey: " ",
+    authDomain: "",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
 };
 
 const Firebase = initializeApp(firebaseConfig);

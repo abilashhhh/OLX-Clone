b87df8b830ff015c6ed0c05bc48843c63998f291
Abilash N <103725378+abilashhhh@users.noreply.github.com>
2024-03-20 20:06:49 +0530

    Removed configurations.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase/app";
 // import 'firebase/storage'
  
  
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: " ",
     authDomain: "",
